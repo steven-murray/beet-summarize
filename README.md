17e53d5a1cd009bad0e71999d362b8ca1d798b76
Steven Murray <steven.murray@curtin.edu.au>
2019-05-06 09:07:26 -0700

    actual readme

--- README.md
+++ README.md
@@ -1,1 +1,117 @@
-beet-summarize
+# Beet Summarize
+
+**Summarize your beets library**
+
+```
+$ beet summarize
+
+genre                  | count
+---------------------- | -----
+Rock                   | 340  
+Classical              | 268  
+Folk                   | 248  
+Pop                    | 248  
+```
+
+``beet-summarize`` is a plugin for the ``beets`` music organisation library,
+that provides the ability to summarize statistics according to fields.
+
+## Installation
+
+$ pip install git+git://github.com/steven-murray/beet-summarize.git
+
+Then add ``summarize`` to your list of ``plugins`` in your beets' ``config.yml``.
+
+## Usage
+
+``beet-summarize`` adds the single sub-command ``summarize``. The interface is
+very simple:
+
+```
+Options:
+  -g GROUP_BY, --group-by=GROUP_BY
+                        field to group by
+  -s STATS, --stats=STATS
+                        stats to display
+  -R, --not-reverse     whether to not reverse the sort
+```
+
+In a bit more detail, ``-g`` will tell ``summarize`` which beets field it will
+summarize over. The default is ``genre``. We could also have grouped by year,
+and used the ``-R`` flag to reverse the results:
+
+```
+$ beet summarize -g year -R
+
+year | count
+---- | -----
+1981 | 1    
+1991 | 4    
+1985 | 9    
+1982 | 10   
+1990 | 11   
+```
+
+Perhaps most importantly, you can specify aggregate statistics to report via 
+``-s`` or ``--stats``. Each statistic is a valid field with optional pre-pending 
+modifiers. Modifiers include an aggregation function (options are ``MIN``, 
+``MAX``, ``SUM``, ``COUNT``, ``AVG``, ``RANGE``), whether to only include 
+``UNIQUE`` entries, and converters for when the field is of str type 
+(options are ``LEN`` and ``WORDS``). The default statistic is ``count``. 
+You can give multiple statistics by enclosing them in quotes. The order of the results will 
+be based on the first given statistic. The format for each statistic is 
+``aggregator<:modifier>|field``, except for ``count`` which is special
+and does not require a field.
+
+As an example:
+
+```
+$ beet summarize -g year -s "count avg|bitrate avg:words|lyrics count:unique|artist"
+
+year | count | avg|bitrate       | avg:words|lyrics   | count:unique|artist
+---- | ----- | ----------------- | ------------------ | -------------------
+2006 | 317   | 648899.5741324921 | 273.51419558359623 | 41                            
+2009 | 244   | 709426.0778688524 | 660.7786885245902  | 17                 
+2005 | 241   | 754819.5145228215 | 681.6099585062241  | 24                 
+2010 | 203   | 747686.5615763547 | 537.1133004926108  | 51    
+```
+
+Here the first statistic is just the number of tracks in the library for each 
+given year (and this sorts the table). The second column is the average 
+bitrate of tracks per year. The third column is the average number of words
+in the lyrics of each track per year. The final column is the number of unique
+artists on tracks per year.
+
+### String conversions
+
+For string-valued fields (eg. artist, lyrics), if the statistic is not ``count``,
+some way to convert the field string to a numerical value is required. A number
+of ways could be thought of, but currently ``summarize`` only supports the
+number of words or number of characters, specified by the modifiers ``words``
+and ``len`` respectively. The latter is default. ``words`` are defined by 
+characters separated by spaces.
+
+### ``unique`` modifier
+
+The ``unique`` modifier is always applied directly on the field. This is usually
+what is wanted: eg. ``count:unique|artist`` produces the number of *different*
+artists. However, it can be a little confusing in scenarios such as 
+``sum:unique:words|lyrics``. This does *not* count the total number of unique
+words in all the lyrics for tracks in the given category. Instead, it applies
+the ``unique`` modifier to the field as a whole. So, it is the sum of the total
+number of words for each track that has unique lyrics. 
+
+## Ideas for improvement
+
+- More string conversions (and perhaps even the ability to not convert from
+  string to number for some aggregators, eg. range could be defined directly
+  on a string by ordering lexically).
+- Ability to specify ``unique`` in such a way that it is applied on elements
+  of the field rather than the entire field, so that eg. getting the total number
+  of unique words in all lyrics for a given year is possible.
+- Multi-level categories, i.e. being able to categorize by genre, then by year
+  within the genre, and provide statistics at each level.
+- Ability to pass an ``-a`` flag to do stats at the album-level. 
+
+
+
--- summarize.py
+++ summarize.py
@@ -25,26 +25,25 @@ def parse_stats(stats):
 
     aggregators = ['min', 'max', 'count', 'sum', 'avg', 'range']
     str_converters = ['len', 'words']
-    
+
     out_dct = {}
     for stat in stats:
         this = out_dct[stat] = {}
 
         # Special case: count
         if stat.lower() == 'count':
-            this['field'] = 'title' # irrelevant
+            this['field'] = 'title'  # irrelevant
             this['aggregator'] = 'count'
             this['str_converter'] = None
             this['unique'] = False
             continue
 
-        
         # Get the field name of the statistic
         this['field'] = stat.split("|")[-1].lower()
-            
+
         # There doesn't *need* to be any modifiers.
         modifiers = stat.split("|")[0].lower() if "|" in stat else []
-        
+
         # Determine the aggregator for this statistic
         aggregator = None
         for agg in aggregators:
@@ -53,18 +52,19 @@ def parse_stats(stats):
                     this['aggregator'] = aggregator = agg
                 else:
                     raise ValueError("You have specified more than one aggregator: {}".format(stat))
-        
+
         # Get str converter
         this['str_converter'] = [m for m in modifiers if m in str_converters]
         if len(this['str_converter']) > 1:
             raise ValueError("You have specified more than one str conversion: {}".format(stat))
         else:
             this['str_converter'] = this['str_converter'][0] if this['str_converter'] else None
-            
+
         # Get specific modifiers
         this['unique'] = "unique" in modifiers
-        
+
     return out_dct, stats[0]
+
 
 def validate_stat(stat, stat_type):
     """Validate a stat dict (from parse_stats), and update it according to the type
@@ -79,8 +79,8 @@ def validate_stat(stat, stat_type):
     # use the length of the string.
     if stat_type is str and not stat['str_converter']:
         stat['str_converter'] = 'len'
-    
-        
+
+
 def group_by(category, items):
     out = {}
     for item in items:
@@ -93,11 +93,6 @@ def group_by(category, items):
 
     return out
 
-def get_item_stat(item, stat):
-    val = getattr(item, stat['field'])
-
-    if type(val) is str:
-        val = derp
 
 def get_items_stat(items, stat):
     if stat['unique']:
@@ -113,19 +108,19 @@ def get_items_stat(items, stat):
             collection.add(val)
         else:
             collection.append(val)
-    
+
     stat_type = type(getattr(items[0], stat['field']))
 
     # We can turn the collection into a list now
     collection = list(collection)
-    
+
     # Convert str stats
     if stat_type is str and stat['aggregator'] != "count":
         if stat['str_converter'] == "len":
             collection = [len(c) for c in collection]
         elif stat['str_converter'] == 'words':
             collection = [len(c.split(" ")) for c in collection]
-    
+
     # Aggregate
     if stat['aggregator'] == 'min':
         return min(collection)
@@ -138,44 +133,46 @@ def get_items_stat(items, stat):
     elif stat['aggregator'] == 'sum':
         return sum(collection)
     elif stat['aggregator'] == 'avg':
-        return sum(collection)/len(collection)
+        return sum(collection) / len(collection)
 
-def print_dct_as_table(keys, dcts, cat_name = None, col_formats=None):
-   """ Pretty print a list of dictionaries (myDict) as a dynamically sized table.
-   If column names (colList) aren't specified, they will show in random order.
-   
-   """
-   columns = list(dcts[0].keys())
 
-   if cat_name:
-       table = [[cat_name] + columns] # 1st row = header
-   else:
-       table = [[""] + columns]
-       
-   for key, item in zip(keys, dcts):
-       table.append(["{}".format(key)] +
-                    ["{val:{fmt}}".format(
-                        val=item[col],
-                        fmt=col_formats[col] if col_formats else "")
-                     for col in columns])
+def print_dct_as_table(keys, dcts, cat_name=None, col_formats=None):
+    """ Pretty print a list of dictionaries (myDict) as a dynamically sized table.
+    If column names (colList) aren't specified, they will show in random order.
 
-   col_size = [max(map(len, col)) for col in zip(*table)]
-   
-   formatStr = ' | '.join(["{{:<{}}}".format(i) for i in col_size])
-   table.insert(1, ['-' * i for i in col_size]) # Seperating line
-   for item in table: print(formatStr.format(*item))
-   
+    """
+    columns = list(dcts[0].keys())
+
+    if cat_name:
+        table = [[cat_name] + columns]  # 1st row = header
+    else:
+        table = [[""] + columns]
+
+    for key, item in zip(keys, dcts):
+        table.append(["{}".format(key)] +
+                     ["{val:{fmt}}".format(
+                         val=item[col],
+                         fmt=col_formats[col] if col_formats else "")
+                         for col in columns])
+
+    col_size = [max(map(len, col)) for col in zip(*table)]
+
+    formatStr = ' | '.join(["{{:<{}}}".format(i) for i in col_size])
+    table.insert(1, ['-' * i for i in col_size])  # Seperating line
+    for item in table: print(formatStr.format(*item))
+
+
 def print_results(res, cat_name, sort_stat, reverse):
-    
-    keys = sorted(res.keys(), key = lambda x: res[x][sort_stat], reverse=reverse)
+    keys = sorted(res.keys(), key=lambda x: res[x][sort_stat], reverse=reverse)
     dcts = [res[key] for key in keys]
 
     print_dct_as_table(keys, dcts, cat_name)
-            
+
+
 def show_summary(lib, query, category, stats, reverse):
     """
     # TODO: albums?
-    """ 
+    """
     items = lib.items(query)
     stats, sort_stat = parse_stats(stats)
 
@@ -190,14 +187,16 @@ def show_summary(lib, query, category, stats, reverse):
 
         for nm, stat in stats.items():
             stat_dct[g][nm] = get_items_stat(items, stat)
-            
-    print_results(stat_dct, category, sort_stat, reverse) 
+
+    print_results(stat_dct, category, sort_stat, reverse)
 
 
 def summarize(lib, opts, args):
     show_summary(lib, decargs(args), opts.group_by, opts.stats, not opts.not_reverse)
-    
+
+
 summarize_command.func = summarize
+
 
 class SuperPlug(BeetsPlugin):
     def commands(self):
